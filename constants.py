2b82b53ce8e728411e9274de89fac6e0414150c5
Alessandro Visintin <alevise.public@gmail.com>
2018-11-27 16:30:45 +0100

    Created file for keeping constants

---
+++ constants.py
@@ -0,0 +1,21 @@
+### Settings ###
+
+# Actions per day to execute
+LIKES = 1500
+FOLLOWS = 250
+UNFOLLOWS = 250
+
+### End of settings  ###
+
+# Timestamp of last action
+LIKE = None
+FOLLOW = None
+UNFOLLOW = None
+
+# 86400 -> seconds in a day 
+# Seconds per like
+SPL = 86400.0 / LIKES
+# Seconds per follow
+SPL = 86400.0 / FOLLOWS
+# Seconds per unfollow
+SPL = 86400.0 / UNFOLLOWS
